ec7ec0bbc064aa569277ebed53b8d8d6d2197e7c
Joao Goncalves <alvesjoaovitor463@gmail.com>
2023-10-20 23:04:37 -0400

    Comentando para imagem na escrita

--- Códigos2023/HW/HW.ino
+++ Códigos2023/HW/HW.ino
@@ -1,13 +1,15 @@
- int val = 5 ;  
- 
+ int sensor = 5; //Definindo porta do sensor 
+ int valor = 0;// variavel que recebera valor do sensor
+
  void setup()  
  {  
-   Serial.begin(9600); // sensor buart rate     
+  Serial.begin(115200);// Iniando a comunicacao serial na frequencia 115200
+  pinMode(sensor, INPUT); //Setando o Pino 5 como entrada
  }  
  void loop()   
  {  
-  val = digitalRead(5); // IR Sensor output pin connected to D1  
-  Serial.println(val);  // see the value in serial m0nitor in Arduino IDE  
+  val = digitalRead(sensor);//Lendo o valor de entrada do sensor e armazenando
+  Serial.println(valor);//Mostrando n monitor serial o valor do sensor   
   delay(100);        
 
-  }  
+}  
