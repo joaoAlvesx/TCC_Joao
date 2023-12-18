38a38657cadca34bfa660c07850d66a5c128ffed
Joao Goncalves <alvesjoaovitor463@gmail.com>
2023-12-17 22:20:08 -0400

    adicionando arquivos pdf e circuitos

--- códigoPrincipal/codigoPrincipal2/codigoPrincipal2.ino
+++ códigoPrincipal/codigoPrincipal2/codigoPrincipal2.ino
@@ -15,10 +15,10 @@
 
 
 // Configração do WiFi
-const char* ssid = "IFMaker Adm"; //VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO EM QUE VAI CONECTAR
-const char* password = "@IFM4k3r"; //VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO EM QUE VAI CONECTAR
+const char* ssid = "KNET-FIBRA-MEL E MEGUI"; //VARIÁVEL QUE ARMAZENA O NOME DA REDE SEM FIO EM QUE VAI CONECTAR
+const char* password = "melemegui"; //VARIÁVEL QUE ARMAZENA A SENHA DA REDE SEM FIO EM QUE VAI CONECTAR
  
-WiFiServer server(880);
+WiFiServer server(80);
 
 //Matriz
 int x = 0;
@@ -39,22 +39,66 @@ int matriz [tamanho][tamanho];
 
 int cenario [tamanho][tamanho] =
 {
-{80,80,80,80,80,80,80,80,80,80,80,80,80,80},
-{80,-99,80,1,1,1,80,80,80,80,80,80,80,80},
-{80,1,80,80,80,1,80,80,80,80,80,80,80,80},
-{80,1,1,1,1,1,1,1,1,1,1,1,80,80},
-{80,1,80,1,80,80,80,80,80,80,1,80,80,80},
-{80,1,1,1,1,1,80,80,80,80,1,80,80,80},
-{80,1,80,80,80,1,80,80,80,80,1,1,1,80},
-{80,1,80,1,80,1,80,80,80,80,1,80,1,80},
-{80,1,80,1,80,1,1,1,1,1,1,80,1,80},
-{80,1,80,1,80,80,80,80,80,1,1,80,1,80},
-{80,1,1,1,1,1,80,80,80,1,1,1,1,80},
-{80,80,80,80,80,1,80,80,80,80,1,1,80,80},
-{80,1,1,1,1,1,1,1,1,1,1,1,999,80},
-{80,80,80,80,80,80,80,80,80,80,80,80,80,80}
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,-99,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+{0,1,1,1,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+{0,0,0,1,0,0,0,0,1,1,1,0,0,0},
+{0,0,0,1,0,0,0,0,1,0,1,0,0,0},
+{0,0,0,1,1,1,1,1,1,0,1,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+{0,0,0,0,0,0,0,0,0,0,1,1,999,0},
+{0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 
 };
+
+void setup()
+{
+  Serial.begin(115200);
+ // system_update_cpu_freq(1680);
+
+
+ 
+  Serial.println("funfou");
+  for (int x = 0; x<tamanho;x++){
+      for (int y = 0; y<tamanho;y++){
+      aux[x][y] = -20;
+       caminho[x][y] = 00;
+      }
+    }
+    
+Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+  Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+  Serial.print("Conectando a "); //ESCREVE O TEXTO NA SERIAL
+  Serial.print(ssid); //ESCREVE O NOME DA REDE NA SERIAL
+  
+  WiFi.begin(ssid, password); //PASSA OS PARÂMETROS PARA A FUNÇÃO QUE VAI FAZER A CONEXÃO COM A REDE SEM FIO
+  
+  while (WiFi.status() != WL_CONNECTED) { //ENQUANTO STATUS FOR DIFERENTE DE CONECTADO
+  delay(500); //INTERVALO DE 500 MILISEGUNDOS
+  Serial.print("."); //ESCREVE O CARACTER NA SERIAL
+  
+  }
+  Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
+  Serial.print("Conectado a rede sem fio "); //ESCREVE O TEXTO NA SERIAL
+  Serial.println(ssid); //ESCREVE O NOME DA REDE NA SERIAL
+  server.begin(); //INICIA O SERVIDOR PARA RECEBER DADOS NA PORTA DEFINIDA EM "WiFiServer server(porta);"
+    yield();
+  Serial.println("Servidor iniciado"); //ESCREVE O TEXTO NA SERIAL
+  
+  Serial.print("IP para se conectar ao NodeMCU: "); //ESCREVE O TEXTO NA SERIAL
+  Serial.print("http://"); //ESCREVE O TEXTO NA SERIAL
+  Serial.println(WiFi.localIP()); //ESCREVE NA SERIAL O IP RECEBIDO DENTRO DA REDE SEM FIO (O IP NESSA PRÁTICA É RECEBIDO DE FORMA AUTOMÁTICA)
+
+   yield();
+
+    
+  }
+
 
 void busca()
 {
@@ -63,37 +107,33 @@ void busca()
   {
     for(y=0;y<tamanho;y++)
     {
-      if(cenario[x][y] == -99)	
+      if(cenario[x][y] == -99)  
       {
         origem[0] = x; 
         origem[1] = y;
         matriz[x][y]=0;
         aux[x][y]=0;
-        
       }
-      if(cenario[x][y] == 999)	
+      if(cenario[x][y] == 999)  
       {
         destino[0] = x; 
         destino[1] = y;
         matriz[x][y]=999;
         aux[x][y]=999;
-
       }
       if(cenario[x][y] == 1)
       {
         matriz[x][y]=-60;
         aux[x][y]=-60;
       }
-      if(cenario[x][y] == 0)	
+      if(cenario[x][y] == 0)  
       {
         matriz[x][y]=-30;
         aux[x][y]=-30;
         
       }
-      
       }
   }
-  
   
   int achou=0;
   while (achou==0)
@@ -124,12 +164,17 @@ void busca()
             b=y;
             aux[x][y]=-99; //-2 na tabela auxiliar significa que foi lido o campo
             
-            if (matriz[a][b+1]==-60)  { matriz[a][b+1]=indice+1;  aux[a][b+1]=indice+1;   }
-            if (matriz[a+1][b]==-60)	{	matriz[a+1][b]=indice+1;	aux[a+1][b]=indice+1; 		}
-            if (matriz[a-1][b]==-60)	{	matriz[a-1][b]=indice+1;	aux[a-1][b]=indice+1; 		}
-            if (matriz[a][b-1]==-60)	{	matriz[a][b-1]=indice+1;	aux[a][b-1]=indice+1;		}
+            if (matriz[a][b+1]==-60) 
+             { matriz[a][b+1]=indice+1;  aux[a][b+1]=indice+1;   }
+            if (matriz[a+1][b]==-60) 
+             { matriz[a+1][b]=indice+1;  aux[a+1][b]=indice+1;     }
+            if (matriz[a-1][b]==-60) 
+             { matriz[a-1][b]=indice+1;  aux[a-1][b]=indice+1;     }
+            if (matriz[a][b-1]==-60) 
+             { matriz[a][b-1]=indice+1;  aux[a][b-1]=indice+1;   }
             
-            if(matriz[a+1][b]==999 || matriz[a][b+1]==999 ||matriz[a-1][b]==999 ||matriz[a][b-1]==999 )	{ Serial.println("achou = 1 ");achou=1;}
+            if(matriz[a+1][b]==999 || matriz[a][b+1]==999 ||matriz[a-1][b]==999 ||matriz[a][b-1]==999 ) 
+             { Serial.println("Destino encontrado");achou=1;}
             cont--;
 
           }
@@ -142,7 +187,7 @@ void busca()
       {
         indice++;
       }
-  }	
+  } 
 
      for (int x = 0; x<tamanho;x++){
       for (int y = 0; y<tamanho;y++){
@@ -167,12 +212,18 @@ void busca()
    
     int busca = 0;
     while(busca<indice)
-    {	busca++;
-        
-        if (matriz[a+1][b]==busca)  { caminho[a+1][b]=busca+1;    a=a+1;  b=b;  } //2
-        else   if (matriz[a][b+1]==busca)  { caminho[a][b+1]=busca+1;    a=a;  b=b+1;  } //3
-        else if (matriz[a-1][b]==busca)	{	caminho[a-1][b]=busca+1;		a=a-1;	b=b;	}		//1
-        else	if (matriz[a][b-1]==busca)	{	caminho[a][b-1]=busca+1;		a=a;	b=b-1;	}	//4
+    { busca++;
+         
+          if (matriz[a][b-1]==busca) 
+         { caminho[a][b-1]=busca+1;    a=a;  b=b-1;  } 
+          else   if (matriz[a][b+1]==busca)  
+         { caminho[a][b+1]=busca+1;    a=a;  b=b+1;  } 
+        else if (matriz[a-1][b]==busca)
+         { caminho[a-1][b]=busca+1;    a=a-1;  b=b;  }   
+        else if (matriz[a+1][b]==busca)  
+         { caminho[a+1][b]=busca+1;    a=a+1;  b=b;  }
+
+         
         //muldar 3 e 4 pra 2 e 3 altera a busca de baixo para lateral
       
     }
@@ -190,55 +241,14 @@ void busca()
  
     
   }
-void setup()
-{
-  Serial.begin(115200);
- // system_update_cpu_freq(1680);
-
-
-  busca();
-  Serial.println("funfou");
-  
-    
-  WiFi.begin(ssid, password); //PASSA OS PAR METROS PARA A FUNÇÃO QUE VAI FAZER A CONEXÃO COM A REDE SEM FIO
- 
-  while (WiFi.status() != WL_CONNECTED) { //ENQUANTO STATUS FOR DIFERENTE DE CONECTADO
-  delay(500); //INTERVALO DE 500 MILISEGUNDOS
-  Serial.print("."); //ESCREVE O CARACTER NA SERIAL
- 
-  }
-  Serial.println(""); //PULA UMA LINHA NA JANELA SERIAL
-  Serial.print("Conectado a rede sem fio "); //ESCREVE O TEXTO NA SERIAL
-  Serial.println(ssid); //ESCREVE O NOME DA REDE NA SERIAL
-  server.begin(); //INICIA O SERVIDOR PARA RECEBER DADOS NA PORTA DEFINIDA EM "WiFiServer server(porta);"
-    yield();
-  Serial.println("Servidor iniciado"); //ESCREVE O TEXTO NA SERIAL
- 
-  Serial.print("IP para se conectar ao NodeMCU: "); //ESCREVE O TEXTO NA SERIAL
-  Serial.print("http://"); //ESCREVE O TEXTO NA SERIAL
-  Serial.println(WiFi.localIP()); //ESCREVE NA SERIAL O IP RECEBIDO DENTRO DA REDE SEM FIO (O IP NESSA PRÁTICA É RECEBIDO DE FORMA AUTOMÁTICA)
-
-
-
-  
-
-
-
-
-    
-  }
-
-
-
 
 
 
 void loop()
 {
- 
-  delay(500);
-  yield();
-  WiFiClient client = server.available(); //VERIFICA SE ALGUM CLIENTE ESTÁ CONECTADO NO SERVIDOR
+  busca();
+  delay(5000);
+   WiFiClient client = server.available(); //VERIFICA SE ALGUM CLIENTE ESTÁ CONECTADO NO SERVIDOR
 
 
   String request = client.readStringUntil('\r'); //FAZ A LEITURA DA PRIMEIRA LINHA DA REQUISIÇÃO
@@ -249,13 +259,41 @@ void loop()
   client.println("");
   client.println("<!DOCTYPE HTML>"); //INFORMA AO NAVEGADOR A ESPECIFICAÇÃO DO HTML
   client.println("<html>"); //ABRE A TAG "html"
-
-  
   client.println("<br>"); 
-    client.println("Cenario");
+   client.println("Cenario"); 
    client.println("<br>"); 
-  
- 
+
+   for (int x = 0; x<tamanho;x++){
+      for (int y = 0; y<tamanho;y++){
+       client.print(" |");
+       client.print(cenario[x][y]);
+    
+       client.print("| ");
+      }
+     client.println("<br>"); 
+    }
+  client.println("<br>");   
+  client.print("Caminho"); 
+  client.println("<br>"); 
+
+    for (int x = 0; x<tamanho;x++){
+      for (int y = 0; y<tamanho;y++){
+       client.print(" |");
+       client.print(caminho[x][y]);
+    
+       client.print("| ");
+      }
+     client.println("<br>"); 
+    }
+   
+    client.println("<br>");
+      client.println("X:");
+      client.print(x);
+      client.println("Y:");
+      client.println(y);
+      client.println("<br>");
+    
+
 
   client.println("</center></h1>"); //ESCREVE "Ola cliente!" NA PÁGINA
   client.println("<center><font size='5'></center>"); //ESCREVE "Seja bem vindo!" NA PÁGINA
